88dffc899d5e2ed5c24efcc8b77821549d1fea6c
Christoph Hugo <christoph.hugo@xing.com>
2018-02-15 13:49:19 +0100

    Update paths and add docker env

--- fish/paths.fish
+++ fish/paths.fish
@@ -3,11 +3,14 @@ set -gx PATH /usr/local/bin $PATH
 set -gx PATH /usr/local/sbin $PATH
 
 # npm
-set -gx PATH /usr/local/share/npm/bin $PATH
+# set -gx PATH /usr/local/share/npm/bin $PATH
 
 # rbenv
 set -gx RBENV_ROOT /usr/local/opt/rbenv
 set -gx PATH $RBENV_ROOT/bin $PATH
 set -gx PATH $RBENV_ROOT/shims $PATH
-
 source (rbenv init - | psub) and rbenv rehash >/dev/null ^&1
+
+# docker
+eval (docker-machine env)
+set -gx DOCKER_IP (docker-machine ip)
